9542a2a90e9c19512ea5867eb10b89c5ea886b22
kom-ops <komaljanwe786@gmail.com>
2019-11-02 17:22:37 +0530

    Added Customer class.

---
+++ TestProject/src/Customer.java
@@ -0,0 +1,18 @@
+
+public class Customer {
+	int CustId;
+	String CustName;
+	public int getCustId() {
+		return CustId;
+	}
+	public void setCustId(int custId) {
+		CustId = custId;
+	}
+	public String getCustName() {
+		return CustName;
+	}
+	public void setCustName(String custName) {
+		CustName = custName;
+	}
+	
+}
